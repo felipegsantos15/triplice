b8c8fd6a054c06d6105c5b0ca84f4cb33778f05a
felipe santos <felipegoncalvesdsantos@gmail.com>
2018-08-27 21:19:04 -0300

    rename routes

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const app = express();
 const path = require('path');
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/template/:value', function (req, res) {
-  res.sendFile(path.join(__dirname, 'app/builder.html'));
+  res.render('builder');
 });
 
-app.get('/templatesView/:value', function (req, res) {
-  res.sendFile(path.join(__dirname, 'templates/template1.html'));
-});
+//app.get('/templatesView/:value', function (req, res) {
+//  res.render('template'+ req.params.value +'.ejs');
+//});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
