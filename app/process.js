9e09595a1580fd11bf440406bb3c5d19e3f46693
felipegsantos <felipegoncalvesdsantos@gmail.com>
2019-02-05 18:30:51 -0200

    update index, get data to process

---
+++ app/process.js
@@ -0,0 +1,35 @@
+
+const router = {
+    createObject: (data) => {
+        return new Promise((resolve, reject) => {
+            let stepsObject = {
+
+            }
+            // result = {};
+            // result.logoImg = '../images/pontal_telecom.png';
+            // result.backgroundImg = "../images/background-happy-woman.jpg";
+            // result.pageTitle = 'A pontal vai alem com você';
+            // result.bodyText = 'A marca Pontal representa o progresso diante de um cenário cada vez mais desafiador: conectar empresas com seus públicos de forma ágil e moderna.';
+            // result.footerImg = '../images/pontal_telecom-logo.png';
+            // result.footerLink = 'www.pontaltelcom.com.br';
+            // result.callMe = true;
+            // result.callMeLater = true;
+            // result.call = true;
+            // result.email = true;
+            // result.whatsapp = true;
+            // result.callMePhrase = 'Ligue-me';
+            // result.callMeLaterPhrase = 'Ligue-me mais tarde';
+            // result.callPhrase = 'Ligar';
+            // result.emailPhrase = 'Envie um email';
+            // result.whatsappPhrase = 'Mande uma mensagem via whatsapp';
+            // result.leftJustify = false;
+            // result.showIcons = false;
+            // result.secundaryColor = "#dddddd";
+            // result.colorPicker = "#ffffff";
+            // result.primaryColor = "#337ab7"; 
+            // result.secundaryColor = '#444444';
+        });
+    }
+}
+
+module.exports = router;
