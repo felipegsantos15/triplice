5f84b4f8a9ad0c0c8336d07155464b19b7b079ae
felipe.santos <felipe.santos@pontaltelecom.com.br>
2020-04-07 21:19:06 -0300

    pegando informações da env

--- app/mysql/mysqlAcess.js
+++ app/mysql/mysqlAcess.js
@@ -2,16 +2,16 @@ const knex = require('knex')({
     client: 'mysql',
     connection: {
         host : process.env.DB_HOST,
-        user : 'root',
-        password : 'q1w2e3',
-        database : 'pontal_admin'
+        user : process.env.USER,
+        password : process.env.PASSWORD,
+        database : process.env.DATABASE
     }
 });
 
 mysqlAcess = {
 	insertTemplate: async (template) => {
 		try {
-            return await knex(`interactive_link_template`).insert(template);
+            return await knex(process.env.TABLE_NAME).insert(template);
 		} catch (error) {
             console.log(error)
 			throw error;
